4c9c85d25307529efb7fc68009dc4f7effcbdca4
Eduardo Ximenes <duduximenes470@gmail.com>
2024-01-10 20:18:53 -0300

    table style

--- resources/views/livewire/customer-navigation.blade.php
+++ resources/views/livewire/customer-navigation.blade.php
@@ -5,26 +5,38 @@
         {{ $this->nextAction }}
     </div>
 
-    <!-- resources/views/livewire/my-table-component.blade.php -->
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th>name</th>
-                    <th>type</th>
-                    <th>email</th>
+     <!-- resources/views/livewire/customer-table.blade.php -->
 
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($customerArray as $data)
-                    <tr>
-                        <td>{{ $data['name'] }}</td>
-                        <td>{{ $data['type'] }}</td>
-                        <td>{{ $data['email'] }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+     <div>
+        <section class="pt-4" >
+            <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
+                <!-- Start coding here -->
+                <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+                    <div class="overflow-x-auto">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="px-4 py-3">Name</th>
+                                    <th scope="col" class="px-4 py-3">Type</th>
+                                    <th scope="col" class="px-4 py-3">Email</th>
+                                    <th scope="col" class="px-4 py-3">Postal Code</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($customerArray as $data)
+                                    <tr class="border-b dark:border-gray-700">
+                                        <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $data['name'] }}</th>
+                                        <td class="px-4 py-3">{{ $data['type'] }}</td>
+                                        <td class="px-4 py-3">{{ $data['email'] }}</td>
+                                        <td class="px-4 py-3">{{ $data['postalCode'] }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </div>
